9556c913c7319a8e72e1ce4562d29164262ac5e0
srigot <sebastien.rigot@gmail.com>
2017-06-09 17:49:45 +0200

    Allow all headers

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(express.static(__dirname + "/public"));
