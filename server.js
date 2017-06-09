086717a8a03aed8406633f3678377a8c8dd58526
srigot <sebastien.rigot@gmail.com>
2017-06-09 17:58:03 +0200

    revert

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
 });
 app.use(express.static(__dirname + "/public"));
