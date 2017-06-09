c76f85682627d6a09835d1116560bca9a75112db
srigot <sebastien.rigot@gmail.com>
2017-06-09 17:36:43 +0200

    Add methods allowed

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var VINS_COLLECTION = "vins" ;
 var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
