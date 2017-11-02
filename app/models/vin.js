b4ccb9c85da5c5927e5c33e8239bb770eaee4182
Sebastien Rigot <sebastien.rigot@atos.net>
2017-11-02 18:01:23 +0100

    Correction

--- app/models/vin.js
+++ app/models/vin.js
@@ -19,12 +19,6 @@ let VinSchema = new Schema(
     finTemp: { type: Number, required: false },
     categorie: { type: String, required: false },
     appellation: { type: String, required: false },
-    emplacement: []
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    year: { type: Number, required: true },
-    pages: { type: Number, required: true, min: 1 },
-    createdAt: { type: Date, default: Date.now },
   },
   {
     versionKey: false
