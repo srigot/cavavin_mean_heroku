50e1c26bc10c864194e58f2b10069dd991d6bdeb
Sebastien Rigot <sebastien.rigot@gmail.com>
2017-12-11 13:24:27 +0100

    Mise a jour tests

--- test/vins.spec.js
+++ test/vins.spec.js
@@ -1,5 +1,5 @@
 //During the test the env variable is set to test
-//process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
 //Require the dev-dependencies
 var chai = require('chai');
@@ -32,25 +32,31 @@ describe('Vins', function () {
       });
     });
     it('it should GET list with one element', function (done) {
-      new vin(require('./jdd/vin1.json')).save().then(function(vin) {
+      let vin1 = require('./jdd/vin1.json');
+      new vin(vin1).save().then(function(vin) {
         chai.request(server)
         .get('/vins')
         .end(function (err, res) {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.eql(1);
+	  res.body[0].nom.should.be.eql(vin1.nom);
+	  res.body[0].annee.should.be.eql(vin1.annee);
           done();
         });
       });
     });
     it('it should GET list with two elements', function (done) {
-      new vin(require('./jdd/vin2.json')).save().then(function (res) {
+      let vin2 = require('./jdd/vin2.json');
+      new vin(vin2).save().then(function (res) {
         chai.request(server)
         .get('/vins')
         .end(function (err, res) {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.eql(2);
+          res.body[1].nom.should.be.eql(vin2.nom);
+	  res.body[1].annee.should.be.eql(vin2.annee);
           done();
         });
       });
